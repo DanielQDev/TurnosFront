17c1c8c28084a7a2911911ce38dfa59f877fc8df
DanielQDev <danielquingaluisadev@gmail.com>
2024-08-07 20:42:39 -0500

    Fix: auth axios intercepros

--- src/modules/auth/api/authApi.ts
+++ src/modules/auth/api/authApi.ts
@@ -4,14 +4,4 @@ const authApi = axios.create({
   baseURL: 'http://127.0.0.1:3000/api/v1'
 })
 
-authApi.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-
-  return config
-})
-
 export { authApi }
