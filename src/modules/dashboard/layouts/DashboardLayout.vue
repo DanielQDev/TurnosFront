9cb826a10b8ed775bbb115f01dc1fa56c19ec929
DanielQDev <danielquingaluisadev@gmail.com>
2024-08-07 20:45:28 -0500

    Add: dashboard and home page.

---
+++ src/modules/dashboard/layouts/DashboardLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col h-full bg-gray-100">
+    <!-- Barra de navegación superior -->
+    <DashboardNavbar />
+
+    <!-- Contenido principal -->
+    <div class="flex-1 flex flex-wrap">
+      <!-- Barra lateral de navegación (oculta en dispositivos pequeños) -->
+      <DashboardSidebar />
+      <!-- Área de contenido principal -->
+      <div class="flex-1 p-4 w-full md:w-1/2">
+        <RouterView />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
+import DashboardNavbar from '../components/DashboardNavbar.vue'
+import DashboardSidebar from '../components/DashboardSidebar.vue'
+</script>
---
+++ src/modules/dashboard/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="scroll-smooth focus:scroll-auto">
+    <DashboardSearch @selected-company="getShifts" />
+    <DashboardTable :shifts="shifts" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import DashboardSearch from '../components/DashboardSearch.vue'
+import DashboardTable from '../components/DashboardTable.vue'
+import { useShiftStore } from '../stores/shift.store'
+
+// const { shifts, getShifts } = useShiftStore()
+const getShifts = (id: number) => {
+  console.log('compa', id)
+}
+</script>
