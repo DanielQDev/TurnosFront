1cd02f2ea8667b0dee62914487c1bf677c1bbfa9
DanielQDev <danielquingaluisadev@gmail.com>
2024-08-18 18:50:52 -0500

    Opt: add timezone.

--- src/utils/getDay.ts
+++ src/utils/getDay.ts
@@ -1,6 +1,7 @@
 export default function getDay(stringDate: string) {
   const date = new Date(stringDate)
-  const dayName = date.toLocaleDateString('es-ES', { weekday: 'long' })
+
+  const dayName = date.toLocaleDateString('es-ES', { weekday: 'long', timeZone: 'UTC' })
 
   return dayName
 }
